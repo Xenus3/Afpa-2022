fe432c653e66faccfda364e390b83c91166f8b7a
KARIM ABERBACH <99872919+Xenus3@users.noreply.github.com>
2022-07-11 11:46:35 +0200

    Add files via upload

---
+++ exercisse-11-7-2022/script.js
@@ -0,0 +1,9 @@
+
+
+
+function auCarre() {
+
+let nombre = document.getElementById("number").value;
+document.getElementById("result").innerHTML = `${nombre*nombre} est le carré de ${nombre}`;
+    
+}
