31e17557b5c20020e1ed98eccef853a8f56aa208
KARIM ABERBACH <99872919+Xenus3@users.noreply.github.com>
2022-07-13 16:46:00 +0200

    Add files via upload

--- Exercisses-DOM/Exercisse-4/index.js
+++ Exercisses-DOM/Exercisse-4/index.js
@@ -1,21 +1,36 @@
+
  
-window.onload = function titre()  {
-    let h1 = document.createElement("h1");
-    let title = document.createTextNode("Michael Jackson Thriller");
+ function titre()  {
+    
+    var h1 = document.createElement("h1");
+    var title = document.createTextNode("Michael Jackson Thriller");
+    h1.style.cssText = "text-align:center;"
     h1.appendChild(title);
     document.body.appendChild(h1);
 
    
 }
 
+titre();
+// variable globale
+paroles = document.createElement("div")
 
-
- function parole() {
-    let paroles = document.createElement("div");
-    let paragraph = document.createElement("p");
-    paroles.appendChild(paragraph)
+function parole() {
+  
+    paroles.className = ("paroles");
+    paroles.style.cssText = 'background-color:light-blue;font-size:55px;border:2px solid black;height:200px;';
     document.body.appendChild(paroles);
+
    
 }
 
 parole();
+
+function paragraphes() {
+    var p1 = document.createElement("p");
+    paroles.appendChild(p1);
+    p1.innerHTML = chanson[0];
+
+}
+
+paragraphes();
