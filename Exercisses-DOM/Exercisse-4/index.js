8203fb2bc009683332d69a12c906cd0dacaf299a
KARIM ABERBACH <99872919+Xenus3@users.noreply.github.com>
2022-07-13 12:04:44 +0200

    Add files via upload

---
+++ Exercisses-DOM/Exercisse-4/index.js
@@ -0,0 +1,21 @@
+ 
+window.onload = function titre()  {
+    let h1 = document.createElement("h1");
+    let title = document.createTextNode("Michael Jackson Thriller");
+    h1.appendChild(title);
+    document.body.appendChild(h1);
+
+   
+}
+
+
+
+ function parole() {
+    let paroles = document.createElement("div");
+    let paragraph = document.createElement("p");
+    paroles.appendChild(paragraph)
+    document.body.appendChild(paroles);
+   
+}
+
+parole();
