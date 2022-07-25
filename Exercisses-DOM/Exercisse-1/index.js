0f74441a43c88523df084a089a3d79aa3b5dc686
KARIM ABERBACH <99872919+Xenus3@users.noreply.github.com>
2022-07-25 13:17:27 +0200

    Delete Exercisses-DOM directory

--- Exercisses-DOM/Exercisse-1/index.js
+++
@@ -1,5 +0,0 @@
-d3.select('h1').text('Rick Astley - Never Gonna Give You Up');
-d3.selectAll('.couplet').text('Were no strangers to love').remove();
-
-d3.select('#erreur').remove();
-d3.select('body').append('footer').text('© Copyright 2020 - Nom');
