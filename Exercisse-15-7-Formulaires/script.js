6cd1d35898cce51cb23b87a15e64452b1ed6805a
KARIM ABERBACH <99872919+Xenus3@users.noreply.github.com>
2022-07-25 13:12:02 +0200

    Delete Exercisse-15-7-Formulaires directory

--- Exercisse-15-7-Formulaires/script.js
+++
@@ -1,6 +0,0 @@
-function concatener() {
-    var nom = document.getElementById("nom").value;
-    var prenom = document.getElementById("prenom").value;
-    document.getElementById("nomcomplet").value = `${nom} ${prenom}`;
-   
-}
