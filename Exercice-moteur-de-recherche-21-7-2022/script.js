d1940467e8963d7e8721ec0b61468a6361d28dc5
KARIM ABERBACH <99872919+Xenus3@users.noreply.github.com>
2022-07-25 13:10:29 +0200

    Add files via upload

---
+++ Exercice-moteur-de-recherche-21-7-2022/script.js
@@ -0,0 +1,13 @@
+function chercherOccurence() {
+var text = document.getElementById("text-area").innerText;
+var input = document.getElementById("search").value;
+
+if(input!="") {
+    var expReg = new RegExp(input, "ig");
+    var nouvelForm = "<span style='background-color:red;'>"+ input +" </span>";
+    text = text.replace(expReg,nouvelForm);
+    document.getElementById("text-area").innerHTML = text;
+}
+
+
+}
